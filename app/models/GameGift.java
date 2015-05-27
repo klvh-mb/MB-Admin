5c512c3561dabc1366b55ff4aad9510f2bfbf05e
keithlei <keithlei01@gmail.com>
2015-05-27 19:17:57 +0800

    minor schema change

--- app/models/GameGift.java
+++ app/models/GameGift.java
@@ -6,9 +6,6 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
@@ -21,12 +18,6 @@ import play.db.jpa.Transactional;
 @Entity
 public class GameGift extends SocialObject {
     private static play.api.Logger logger = play.api.Logger.apply(GameGift.class);
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long id;
-    
-    public String name;
     
     @Lob
 	public String description;
